90711b50ac5bbd96c1264b1f5bb86689e43fade7
Yangyang <yeongyeonglung@gmail.com>
2026-03-16 16:19:00 -0400

    Initial Commit

---
+++ Yarn&YarnTests/Yarn_YarnTests.swift
@@ -0,0 +1,17 @@
+//
+//  Yarn_YarnTests.swift
+//  Yarn&YarnTests
+//
+//  Created by Yangyang Long on 3/16/26.
+//
+
+import Testing
+@testable import Yarn_Yarn
+
+struct Yarn_YarnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
